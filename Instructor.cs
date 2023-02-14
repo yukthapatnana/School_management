56120d00a746df8f8c5726be7a803a842df1b8fd
yukthapatnana <124807057+yukthapatnana@users.noreply.github.com>
2023-02-14 16:15:10 +0530

    Add files via upload
    Constructor

---
+++ Instructor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace day7
+{
+    internal class Instructor
+    {
+        public string instructorName;
+        public float avgFeedback;
+        public int experience;
+        public string[] instructorskills;
+
+        public Instructor(string name, float feedback, int exp, string[] skills)
+        {
+            instructorName = name;
+            avgFeedback = feedback;
+            experience = exp;
+            instructorskills = skills;
+        }
+
+        public bool ValidateEligibility()
+         
+        {
+            if (experience > 3 && avgFeedback >= 4.5)
+            {
+                return true;
+            }
+            else if (experience <= 3 && avgFeedback >= 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool CheckSkill(string technology)
+        {
+            if (ValidateEligibility())
+            {
+                foreach (string skill in instructorskills)
+                {
+                    if (technology.Equals(skill))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace day7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] instructorskills = {"python","java","C#"};
+            Instructor instructor = new Instructor("pinky", 4f,3, instructorskills);
+            if (instructor.ValidateEligibility())
+            {
+                Console.WriteLine("Instructor is eligible.");
+                if (instructor.CheckSkill("C#"))
+                {
+                    Console.WriteLine("Instructor has the required skill.");
+                }
+                else
+                {
+                    Console.WriteLine("Instructor does not have the required skill.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Instructor is not eligible.");
+            }
+        }
+    }
+
+}
